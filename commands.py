5e358b7f15d3e1a59645c57797cdb401f619956b
Jocelyn Guan <160393656+jocelyng012@users.noreply.github.com>
2024-03-05 11:07:31 -0800

    FiveTwelve

---
+++ commands.py
@@ -0,0 +1,8 @@
+"""Commands as communicated to the game manager from a controller component"""
+
+LEFT = "Left"
+RIGHT = "Right"
+UP = "Up"
+DOWN = "Down"
+UNMAPPED = "Unmapped"
+CLOSE = "Close"    # When the window is closed
